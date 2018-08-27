8690d5a8ba2c756f14c84efded1262f46aa83c2b
Bon20 <41635372+Bon20@users.noreply.github.com>
2018-08-27 16:25:13 +0800

    Add files via upload

---
+++ PROG_EXER3.cpp
@@ -0,0 +1,17 @@
+//Bags of Rice
+//Bonifacio M. De Robles Jr.
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+float pounds,SOR,rslt;
+pounds=2205;
+cout<< " Type the amount" << endl;
+cin>> SOR;
+cout<<endl;
+rslt=pounds/SOR;
+cout<<rslt;
+return 0;
+}
---
+++ PROG_EXER4.cpp
@@ -0,0 +1,16 @@
+//Bonifacio M. De Robles Jr.
+//Force
+#include<iostream>
+ #include<cmath>
+  using namespace std; 
+  int main() 
+  { 
+  double k,m1,m2,d,f; k=0.0000000667; 
+  f=((m1*m2)/(d*d))*k;
+   cout << "m1 =" << endl; cin >> m1; 
+   cout << "m2 =" <<endl; cin >> m2; 
+   cout << "d =" << endl; cin >> d; 
+   cout << "Force is equal to:"<<f<<endl; 
+   return 0;
+  }
+
